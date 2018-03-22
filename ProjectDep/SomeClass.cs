d28e5bb3247cd2dcdf63fb54f8d0fe049531a3c8
Mirko Velic <mirko.velic@inlogik.com>
2018-03-22 16:34:48 +1100

    Initial

---
+++ ProjectDep/SomeClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ProjectDep
+{
+    public class SomeClass
+    {
+        public static void AMethodThatCallsSqlClient()
+        {
+            var whatever = SqlClientFactory.Instance;
+            Console.WriteLine(whatever.ToString());
+        }
+    }
+}
